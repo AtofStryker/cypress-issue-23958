76b53b143c4055ea876cf4e99c59efdf18561209
Bill Glesias <bglesias@gmail.com>
2023-01-09 15:15:17 -0500

    add visit/soap sample tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,19 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      // on('before:browser:launch', (browser = {}, launchOptions) => {
-      //   /* ... */
-      //   launchOptions.preferences["network.cookie.sameSite.laxByDefault"] = true
-      //   launchOptions.preferences["network.cookie.sameSite.noneRequiresSecure"] = true
-      //   console.log(JSON.stringify(launchOptions.preferences))
-      // })
-    },
     baseUrl: "https://cypress-dev-ed.develop.lightning.force.com",
     experimentalModifyObstructiveThirdPartyCode: true,
-    experimentalUseDefaultDocumentDomain: true,
-    // modifyObstructiveCode: true,
-    // chromeWebSecurity: false,
+    experimentalSkipDomainInjection: ['*.salesforce.com', '*.force.com']
   },
 });
--- cypress/e2e/salesforce-visit.cy.js
+++ cypress/e2e/salesforce-visit.cy.js
@@ -1,12 +1,11 @@
 describe("navigates to set site", () => {
-  it("does not work in chrome, but does in firefox", () => {
+  it("works with cy.origin()", () => {
     cy.visit("/");
     cy.get("#username").type(Cypress.env("sf_username"));
     cy.get("#password").type(Cypress.env("sf_password"), {
       log: false,
     });
     cy.get("#Login").click();
-    // NOTE: this is not a pattern we want to encourage. We should be able to set base url and immediately hop into a cy.origin
     cy.origin("https://cypress-dev-ed.develop.lightning.force.com", () => {
       cy.get(".title.slds-truncate").first().should("have.text", "Home");
     });
